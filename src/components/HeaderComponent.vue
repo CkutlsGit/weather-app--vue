602f82980cc14a4ca737d2e79c9b7c6e4ac8fc64
Kostya <ckutls228@gmail.com>
2024-03-12 21:38:16 +0300

    added logic for headercomponent

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -5,15 +5,50 @@
         <h1 class="capitalize text-2xl max-sm:my-2">weather vue</h1>
       </div>
       <div class="header__location">
-        <button class="text-xl bg-cyan-900 px-2 py-2 rounded-md hover:text-gray-400 ease-in duration-300 max-sm:px-1 max-sm:py-1 max-sm:text-lg">Choose a Location</button>
+        <button
+            class="text-xl bg-cyan-900 px-2 py-2 rounded-md hover:text-gray-400 ease-in duration-300 max-sm:px-1 max-sm:py-1 max-sm:text-lg"
+            @click="clickBtn = true"
+            v-if="cityId === ''"
+        >
+          Choose a Location
+        </button>
+        <h1 @click="clickBtn = true" class="text-xl bg-cyan-900 px-2 py-2 rounded-md hover:text-gray-400 ease-in duration-300 max-sm:px-1 max-sm:py-1 max-sm:text-lg capitalize cursor-pointer">{{ cityId }}</h1>
       </div>
     </div>
   </header>
+  <inputcity-component v-if="clickBtn" @closeinput="closeInput" @getcity="getCityId"></inputcity-component>
 </template>
 
 <script>
-export default {
+import InputcityComponent from "@/components/InputcityComponent.vue";
 
+import { ref, onMounted } from 'vue'
+import { GetCookie } from "../../composables/CookieStorage";
+
+export default {
+  components: { InputcityComponent },
+  setup() {
+    const clickBtn = ref(false)
+    const cityId = ref('')
+
+    onMounted(() => {
+      if(GetCookie('currentcity')) {
+        cityId.value = GetCookie('currentcity')
+      }
+    })
+
+    function closeInput() {
+      clickBtn.value = false
+    }
+
+    function getCityId(cityIdGet) {
+      cityId.value = cityIdGet
+      clickBtn.value = false
+      console.log(cityId.value)
+    }
+
+    return { clickBtn, cityId,closeInput, getCityId }
+  }
 }
 </script>
 
