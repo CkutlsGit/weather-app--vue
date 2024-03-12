fd45f933e9f0051af5707e77e13fb791881cc334
Kostya <ckutls228@gmail.com>
2024-03-12 19:57:58 +0300

    added custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      'dark-blue': '#1D2837'
+    }
   },
   plugins: [],
 }
