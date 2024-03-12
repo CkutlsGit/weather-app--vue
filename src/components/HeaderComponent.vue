b94aa20adb484c747b2b4996d920ee0cf8feaf81
Kostya <ckutls228@gmail.com>
2024-03-12 20:24:16 +0300

    added styles for header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,6 +1,13 @@
 <template>
-  <header class="header">
-
+  <header class="header bg-dark-blue">
+    <div class="header__content flex justify-between items-center px-16 py-8 text-white max-sm:flex-col">
+      <div class="header__logo">
+        <h1 class="capitalize text-2xl max-sm:my-2">weather vue</h1>
+      </div>
+      <div class="header__location">
+        <button class="text-xl bg-cyan-900 px-2 py-2 rounded-md hover:text-gray-400 ease-in duration-300 max-sm:px-1 max-sm:py-1 max-sm:text-lg">Choose a Location</button>
+      </div>
+    </div>
   </header>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
-    colors: {
-      'dark-blue': '#1D2837'
-    }
   },
   plugins: [],
 }
