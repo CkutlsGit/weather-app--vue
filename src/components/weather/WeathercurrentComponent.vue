3ed6ecb7d5f350368311035aa5585231072300cb
Kostya <ckutls228@gmail.com>
2024-03-16 10:48:23 +0300

    added logic for request and get weather-current in currentcity

--- src/components/weather/WeathercurrentComponent.vue
+++ src/components/weather/WeathercurrentComponent.vue
@@ -7,23 +7,47 @@
 <script>
 import {onMounted, ref, watch} from "vue";
 import {eventBus} from "@/main";
+import {GetCurrentWeather} from "../../../composables/WeatherAPI";
 
 export default {
   props: ['city'],
   setup(props) {
     const cityCurrent = ref('')
+    const WeatherinCity = ref('')
 
-    watch(() => props.city, (newValue) => {
+    watch(() => props.city, async newValue => {
       cityCurrent.value = newValue
+
+      const response = await GetCurrentWeather(cityCurrent.value)
+      const data = {
+        summary: response.data.current.summary,
+        temperature: response.data.current.temperature,
+        icon_num: response.data.current.icon_num
+      }
+
+      WeatherinCity.value = data
+
+      console.log(WeatherinCity)
     })
 
     onMounted(() => {
-      eventBus.on('getcity', (cityUpdate) => {
+      eventBus.on('getcity', async (cityUpdate) => {
         cityCurrent.value = cityUpdate
+
+        const response = await GetCurrentWeather(cityUpdate)
+        const data = {
+          summary: response.data.current.summary,
+          temperature: response.data.current.temperature,
+          icon_num: response.data.current.icon_num
+        }
+
+        WeatherinCity.value = data
       })
     })
 
-    return { cityCurrent }
+
+
+    return { cityCurrent, WeatherinCity }
   }
 }
 </script>
