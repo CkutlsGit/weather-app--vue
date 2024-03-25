2701f9e26b24f563903412d235ce58240fef55d6
Kostya <ckutls228@gmail.com>
2024-03-25 10:58:46 +0300

    added started files for workign

---
+++ weather-app/components/HeaderComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="header bg-dark-blue">
+    <div class="header__content flex justify-between items-center px-16 py-8 text-white max-sm:flex-col">
+      <div class="header__logo">
+        <h1 class="capitalize text-2xl max-sm:my-2">weather vue</h1>
+      </div>
+      <div class="header__location">
+        <button
+            class="text-xl bg-cyan-900 px-2 py-2 rounded-md hover:text-gray-400 ease-in duration-300 max-sm:px-1 max-sm:py-1 max-sm:text-lg"
+            @click="clickBtn = true"
+            v-if="cityId === ''"
+        >
+          Choose a Location
+        </button>
+        <h1 v-else @click="clickBtn = true" class="text-xl bg-cyan-900 px-2 py-2 rounded-md hover:text-gray-400 ease-in duration-300 max-sm:px-1 max-sm:py-1 max-sm:text-lg capitalize cursor-pointer">{{ cityId }}</h1>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script setup>
+  const clickBtn = ref(false)
+  const cityId = ref('')
+
+  
+</script>
+
+<style>
+
+</style>
---
+++ weather-app/layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <header-component/>
+  <div class="container mx-auto p-4">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
