9198eaaf7fc0e5c67988fbd8e5280d8250250567
Kostya <ckutls228@gmail.com>
2024-03-25 15:47:12 +0300

    changed stack site for footercomponent

--- weather-app/components/FooterComponent.vue
+++ weather-app/components/FooterComponent.vue
@@ -5,7 +5,7 @@
       <div class="footer-text__content text-white flex flex-col mx-auto w-2/5 text-center py-6">
         <a class="py-2 hover:text-gray-400 ease-in duration-200" href="https://github.com/CkutlsGit">by Konstantin aka Ckutls</a>
         <a class="hover:text-gray-400 ease-in duration-200" href="https://rapidapi.com/MeteosourceWeather/api/ai-weather-by-meteosource">When using the Weather API</a>
-        <h2 class="py-2">Site stack: Vue3(Composition API), Tailwind CSS, JS(ES6)</h2>
+        <h2 class="py-2">Site stack: Nuxt.js 3, Vite, Tailwind css, JS(ES6)</h2>
       </div>
     </div>
   </footer>
