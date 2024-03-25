474a516b81f93ac696639b2b68e1c2839ee94042
Kostya <ckutls228@gmail.com>
2024-03-25 13:57:28 +0300

    added vision for img

--- weather-app/components/weather/WeathercurrentComponent.vue
+++ weather-app/components/weather/WeathercurrentComponent.vue
@@ -3,7 +3,7 @@
     <div class="weather-current__content bg-dark-blue text-center text-white my-16 w-2/12 rounded-md max-sm:w-6/12">
       <h2 class="text-lg font-bold capitalize my-2">{{ cityCurrent }}</h2>
       <div class="weather-current__info flex flex-col justify-center text-base font-medium">
-        <img class="mx-auto w-12 h-12" :src="imgSrc" :alt="WeatherinCity.summary">
+        <img class="mx-auto w-12 h-12" :src="iconSrc" :alt="WeatherinCity.summary">
         <h2 class="mt-1">{{ WeatherinCity.summary }}</h2>
         <h2 class="my-2">{{ WeatherinCity.temperature }} °C</h2>
       </div>
@@ -20,7 +20,7 @@
     temperature: '',
     icon_num: ''
   })
-  const imgSrc = ref('')
+  const iconSrc = ref('')
 
   onMounted(() => {
     $bus.on('sendCityValue',async (cityValue) => {
@@ -32,7 +32,7 @@
       WeatherinCity.temperature = data.current.temperature.toFixed(0)
       WeatherinCity.icon_num = data.current.icon_num
 
-      // imgSrc.value = require('@/assets/img/icons-weather' + WeatherinCity.icon_num + '.png')
+      iconSrc.value = `/icons-weather/${ WeatherinCity.icon_num }.png`
     })
   })
 </script>
